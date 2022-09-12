de9754b311662b4e21b5f27b5fd5ea6f09d4b000
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-12 16:38:44 -0700

    some basic styles applied

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,8 +1,10 @@
+import './Animal.css';
+
 export default function Animal(props) {
   return (
     <>
-      <div className='animal'>
-        <p>{props.animal.name}</p>
+      <div className='animal' style={{ top: props.animal.top, left: props.animal.left }}>
+        <p className='name'>{props.animal.name}</p>
         <p>{props.animal.says}</p>
       </div>
     </>);
