bdac1189c6c2e3bdf9b0fb3530ba3e8cb6ccf227
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-12 16:42:16 -0700

    animal images render

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,6 +4,7 @@ export default function Animal(props) {
   return (
     <>
       <div className='animal' style={{ top: props.animal.top, left: props.animal.left }}>
+        <img src={`${process.env.PUBLIC_URL}/animals/${props.animal.type}.svg`}/>
         <p className='name'>{props.animal.name}</p>
         <p>{props.animal.says}</p>
       </div>
