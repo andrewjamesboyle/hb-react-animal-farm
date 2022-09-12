cb4b0b7e0f7d8a3f7ddb5f64f437996d01827599
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-12 16:09:25 -0700

    animal info renders

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <>
+      <div className='animal'>
+        <p>{props.animal.name}</p>
+        <p>{props.animal.says}</p>
+      </div>
+    </>);
 }
